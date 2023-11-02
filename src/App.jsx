4450fb7ef43b6355f24d1ad44cedb5ac54cfd66d
vba16 <vba16@sfu.ca>
2023-11-01 19:29:35 -0700

    fixed some css and centered somme content

--- src/App.jsx
+++ src/App.jsx
@@ -12,41 +12,35 @@ export default function App(){
       setNewTexts((currentTexts) => {
         return [...currentTexts, {id: crypto.randomUUID(), title: newItem, completed: false},]
       })
-<<<<<<< HEAD
-    
-      setNewItem("")
-    }
-    
-    
-=======
     }
 
     setNewItem("")
->>>>>>> 96a0a7c0f565c2a718e9d0364b64086d8a2cad79
     
   }
 
   return <>
-    <form onSubmit={handleAdd}>
-      <div className="form">
-      <label className="title" htmlFor="" >Type anything</label>
-      <br />
-      <div className="type-and-add">
-        <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" className="type-text"/>
-        <button className="add-button">Add</button>
-      </div>
+    <div className="div-title">
+      <label className="title" htmlFor="" >Chatroom</label>
+    </div>
       
-      </div>
-    </form>
+    <div className="div-form">
+      <form onSubmit={handleAdd} className="form-element">
+        <div className="form-content">
+        
+        <br />
+        <div className="type-and-add">
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" className="type-text"/>
+          <button className="add-button">Add</button>
+        </div>
+        
+        </div>
+      </form>
+    </div>
+    
     <ul className="all-user-messages">
       {texts.map(text => {
         return <>
-<<<<<<< HEAD
-        <label key={text.id}>{text.title}</label>
-        <br />
-=======
         <li className="user-messages">{text.title}</li>
->>>>>>> 96a0a7c0f565c2a718e9d0364b64086d8a2cad79
         </>
       })}
     </ul>
